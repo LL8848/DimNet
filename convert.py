5cc372cfdb1129ef38a2aa48befb83f9041a9e9f
Lin <lnl5@NIST.GOV>
2022-04-15 15:04:32 -0400

    Update convert.py

--- convert.py
+++ convert.py
@@ -35,28 +35,28 @@ def dimnet_to_correlation(model, verbosity=0):
     xscaler = model.xscaler
     yscaler = model.yscaler
     net = model.net
-    
+
     # w1, w2, w3, b1, b2, b3: weight and bias of the DimNet
     with torch.no_grad():
         net.cpu()
         w1 = net.layer1.weight.numpy()
         b1 = net.layer1.bias.numpy()
-        
+
         w2 = net.layer2.weight.numpy()
         b2 = net.layer2.bias.numpy()
-        
+
         w3 = net.output.weight.numpy()
         b3 = net.output.bias.numpy()
-    
+
     # u, s: vectors of the mean and the standard deviation of training samples
-    u = xscaler.mean_   
+    u = xscaler.mean_
     s = xscaler.scale_
-    
+
     # y_factor: scaling factor for y (y_factor = y_max / eps_max)
     y_max = yscaler.scale_[0]
     y_factor = y_max
-    
-    
+
+
     if (verbosity > 2):
         print("-"*20)
         print("DimNet Parameters")
@@ -68,7 +68,7 @@ def dimnet_to_correlation(model, verbosity=0):
         print("w2:")
         print(w2)
         print("b2:")
-        print(b2) 
+        print(b2)
         print("w3:")
         print(w3)
         print("b3:")
@@ -82,41 +82,41 @@ def dimnet_to_correlation(model, verbosity=0):
         print(s)
         print("y_factor:")
         print(y_factor)
-    
-    
+
+
     pass # placeholder for the conversion calculation
-    
+
     print("")
     print("-"*20)
     print("Algebraic Correlation")
     print("-"*20)
-    
+
     """
     1D example
-    
+
     z1 = log(Re**0.48) - 2.2840
     z2 = log(Re**-0.46) + 3.8497
-    
+
     1) for z1 > 0, z2 > 0:
     -2.1e+03 + 6.2e+02 * (Re**-1.00) - 1.1e-21 * (Re**5.60)
-    
+
     2) for z1 > 0, z2 <= 0:
     -2.1e+03 + 8.3e-01 * (Re**-0.45) - 5.0e-02 * (Re**-0.10)
-    
+
     ...
-    
+
     -----------------------------------------------------------------------
     2D example
-    
+
     z1 = log(Re**0.48 * K**0.01) - 2.2840
     z2 = log(Re**-0.46 * K**0.02) + 3.8497
-    
+
     1) for z1 > 0, z2 > 0:
     -2.1e+03 + 6.2e+02 * (Re**-1.00 * K**0.03) - 1.1e-21 * (Re**5.60 * K**0.03)
-    
+
     2) for z1 > 0, z2 <= 0:
     -2.1e+03 + 8.3e-01 * (Re**-0.45 * K**0.03) - 5.0e-02 * (Re**-0.10 * K**0.03)
-    
+
     ...
     """
 
@@ -130,15 +130,9 @@ def pw_predict():
 
 
 if __name__ == '__main__':
-    path = 'test_pw/Clean_Smooth_200_[2, 1]_75.model'
+    path = 'data/Noisy_Rough_1200_[2, 2]_5.model'
     model = load(path)
     dimnet_to_correlation(model, verbosity=5)
-    
-    # compare preditions by the original DimNet and by the algebraic correlation
-    
-    # 1D example
-    df1 = pd.read_csv('data_clean.csv')
-    # y_dimnet = model.predict()
-    
-    
+
+
     # 2D example
